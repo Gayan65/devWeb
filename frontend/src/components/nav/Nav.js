121ff4f7216e0b8392e6de9e4af16aae8a3eba15
gayan65728@yahoo.com <egawi001@edu.xamk.fi>
2024-02-15 04:53:50 +0200

    basic nav config

--- frontend/src/components/nav/Nav.js
+++ frontend/src/components/nav/Nav.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Nav = () => {
-    return <div>Nav</div>;
+    return (
+        <div>
+            <nav className="navbar">
+                <div className="nav-logo">Logo</div>
+                <ul className="nav-ul-list">
+                    <li className="nav-list">About Us</li>
+                    <li className="nav-list">Contact Us</li>
+                    <li className="nav-list">Services</li>
+                    <li className="nav-list">Pricing</li>
+                </ul>
+                <div className="nav-login">Login</div>
+            </nav>
+        </div>
+    );
 };
 
 export default Nav;
