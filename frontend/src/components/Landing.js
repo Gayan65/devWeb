683890b03d4d1990f1372c48a8f8c780f5732059
gayan65728@yahoo.com <egawi001@edu.xamk.fi>
2024-02-17 10:24:44 +0200

    hedder1 and 2 add

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -5,11 +5,12 @@ const Landing = () => {
         <div>
             <div className="shape-obj-one">
                 <div>
-                    <p className="hero-landing-header-1">
-                        Unlocking Digital <br />{" "}
-                        <span className="hero-landing-header-1-briliance">
-                            Brilliance
-                        </span>
+                    <p className="hero-landing-header-1">Unlocking Digital</p>
+                    <span className="hero-landing-header-1-briliance">
+                        Brilliance
+                    </span>
+                    <p className="hero-landing-header-2">
+                        "Powerful solutions for every need, crafted in code."
                     </p>
                 </div>
             </div>
