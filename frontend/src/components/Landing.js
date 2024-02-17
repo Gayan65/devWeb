3c905ae2c690146073ac9e1ba84fa045ef452a93
gayan65728@yahoo.com <egawi001@edu.xamk.fi>
2024-02-17 04:15:01 +0200

    Landing page hero section moto add

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -3,7 +3,16 @@ import React from "react";
 const Landing = () => {
     return (
         <div>
-            <div className="shape-obj-one"></div>
+            <div className="shape-obj-one">
+                <div>
+                    <p className="hero-landing-header-1">
+                        Unlocking Digital <br />{" "}
+                        <span className="hero-landing-header-1-briliance">
+                            Brilliance
+                        </span>
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
