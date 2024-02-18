79af8149f8b9dd0f4c27f536740c29b3ae4a03be
Gayan65 <gayan65728@yahoo.com>
2024-02-18 14:56:44 +0200

    Basic nav bar media q add

--- frontend/src/components/nav/Nav.js
+++ frontend/src/components/nav/Nav.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Nav = () => {
+    const [isToggled, setToggled] = useState(false);
+
+    const handleToggle = () => {
+        setToggled(!isToggled);
+    };
     return (
         <div>
-            <header>
+            <header className="desktop-header">
                 <div className="nav-logo">
                     <span className="logo-letters">tech</span>Hive
                 </div>
@@ -23,8 +28,33 @@ const Nav = () => {
                         </li>
                     </ul>
                 </nav>
-                <button className="cta">Login</button>
+                <button className="cta nav-login">Login</button>
             </header>
+            {/* Mob Nav version start */}
+            <div className="mobnav">
+                {isToggled && (
+                    <nav>
+                        <ul className="nav-links ">
+                            <li>
+                                <a href="#">About Us</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact Us</a>
+                            </li>
+                            <li>
+                                <a href="#">Services</a>
+                            </li>
+                            <li>
+                                <a href="#">Pricing</a>
+                            </li>
+                            <li>
+                                <button className="cta">Login</button>
+                            </li>
+                        </ul>
+                    </nav>
+                )}
+                <div className="nav-toggler" onClick={handleToggle}></div>
+            </div>
         </div>
     );
 };
